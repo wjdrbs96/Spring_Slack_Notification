e1f58421f693ac61674e2c643537b5feb51ea158
Gyunny <wjdrbs966@naver.com>
2021-05-22 01:33:43 +0900

    Interceptor Logging

--- src/main/java/com/example/springslack/config/LoggingInterceptor.java
+++ src/main/java/com/example/springslack/config/LoggingInterceptor.java
@@ -1,8 +1,12 @@
 package com.example.springslack.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,17 +14,26 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * created by jg 2021/05/20
  */
+@RequiredArgsConstructor
 @Slf4j
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
 
+    private final ObjectMapper objectMapper;
+
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         log.error("==============");
-        log.error("Request URI : " + request.getRequestURI());
-        log.error("Query String : " + request.getQueryString());
-        log.error("Request URL : " + request.getRequestURL());
-        log.error("Request Method : " + request.getMethod());
+        log.error("Query String : " + request.getQueryString() + " || Request URL : " + request.getRequestURL() + " || Request Method : " + request.getMethod());
         log.error("==============");
+
+        if (request.getMethod().equals("POST")) {
+            final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+            final ContentCachingResponseWrapper cachingResponse = (ContentCachingResponseWrapper) response;
+            log.error(
+                    String.format("Request Body : %s", objectMapper.readTree(cachingRequest.getContentAsByteArray())),
+                    String.format("Response Body : %s", objectMapper.readTree(cachingResponse.getContentAsByteArray()))
+            );
+        }
     }
 }
--- src/main/java/com/example/springslack/controller/TestController.java
+++ src/main/java/com/example/springslack/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.example.springslack.controller;
 
+import com.example.springslack.dto.TestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +20,10 @@ public class TestController {
         log.info("Bobae is Babo");
         log.debug("MARU Server");
         return "test";
+    }
+
+    @PostMapping("/test")
+    public TestDto testPost(@RequestBody TestDto testDto) {
+        return testDto;
     }
 }
